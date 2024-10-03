ee2b7042bcb71539d5582ec67d60f8a4e05fc0a4
Abdul SUbhan Sajid <abdulsubhan.sajid@enigmatixglobal.com>
2024-10-04 02:08:23 +0500

    intial commit

---
+++ src/lib/firebase.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCpjy2drXUrli6W5txafhUrTdchp6iyiUU",
+  authDomain: "blog-f7e8d.firebaseapp.com",
+  projectId: "blog-f7e8d",
+  storageBucket: "blog-f7e8d.appspot.com",
+  messagingSenderId: "439966660046",
+  appId: "1:439966660046:web:bf9035d7c22669da0b279f",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
