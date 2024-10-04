d044daf33b882744d1cc6fb351197685430f0a04
Abdul Rehman <existed08@gmail.com>
2024-10-05 03:08:36 +0500

    ref: remove extra files

--- generated/javascript/default/esm/index.esm.js
+++
@@ -1,8 +0,0 @@
-import { getDataConnect, validateArgs } from 'firebase/data-connect';
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'blog-app',
-  location: 'us-central1'
-};
-
--- generated/javascript/default/index.cjs.js
+++
@@ -1,9 +0,0 @@
-const { getDataConnect, validateArgs } = require('firebase/data-connect');
-
-const connectorConfig = {
-  connector: 'default',
-  service: 'blog-app',
-  location: 'us-central1'
-};
-exports.connectorConfig = connectorConfig;
-
